b6aa82df84b33a6a81029cc80ab231b40e3bf3b7
woody180 <woody180@rocketmail.com>
2022-10-22 14:40:18 +0400

    html helper & configs

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -2,13 +2,20 @@
 
 <?php $this->start('mainSection') ?>
 <section class="uk-section">
-    <div class="uk-container uk-container-small">
+    <div class="uk-container">
 
         <?= getFlashData('message') ?>
         
         <h1><?= $title ?></h1>
         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo asperiores beatae officia accusamus quibusdam nobis ipsum repudiandae. Corporis odit voluptatem eveniet modi unde, nam fuga blanditiis harum, ut delectus optio.</p>    
     
+        <?= img([
+            'src' => 'images/octopus.png',
+            'class' => 'uk-border-rounded uk-display-block',
+            'alt' => 'alt tag',
+            'width' => 300
+        ]) ?>
+
     </div>
 </section>
 
